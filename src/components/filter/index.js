f8f5dd47bc230e46ba55ae7f7137a12ec945fc70
Caio César Silva <caiocesarsilva08@gmail.com>
2020-05-19 21:14:06 -0300

    Filter Component
    I started to write the implementation of the filter component in the state. Now, the options are mapped items.

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -1,11 +1,46 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as filterCategories from "../../redux/visibility-filter/filterCategories";
+//import * as filterActions from "../../redux/visibility-filter/filterActionTypes";
 
-export const Filter = () => (
+export const Filter = ({ activeFilter }) => (
   <>
     <h3>Show</h3>
-    <span href="#">All</span> | <a href="teste">Done</a> |{" "}
-    <a href="teste">To do</a>
+    {filterItems.map((item) => {
+      if (item.action === activeFilter) {
+        return (
+          <span key={item.action} style={{ marginRight: 10 }}>
+            {item.label}
+          </span>
+        );
+      }
+
+      return (
+        <a href="teste" key={item.action} style={{ marginRight: 10 }}>
+          {item.label}
+        </a>
+      );
+    })}
   </>
 );
 
-export default Filter;
+const filterItems = [
+  {
+    label: "All",
+    action: filterCategories.SHOW_ALL,
+  },
+  {
+    label: "Done",
+    action: filterCategories.SHOW_DONE,
+  },
+  {
+    label: "To do",
+    action: filterCategories.SHOW_UNDONE,
+  },
+];
+
+const mapStateToProps = (state) => ({
+  activeFilter: state.visibilityFilter,
+});
+
+export default connect(mapStateToProps)(Filter);
