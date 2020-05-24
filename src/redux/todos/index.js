b77a86c1f26b7ec2fde1dc7ce1bc1efd2f39f671
Caio César Silva <caiocesarsilva08@gmail.com>
2020-05-24 17:56:11 -0300

    createReducer
    I created and implemented a function to createReducers, also modified the todosReducer and the visibilityReducer to use this function. With this, i reduced the boilerplate in the application code.

--- src/redux/todos/index.js
+++ src/redux/todos/index.js
@@ -1,7 +1,27 @@
 import * as todosActionsTypes from "./todosActionTypes";
-
+import createReducer from "../createReducer";
 export const initialState = [];
 
+const todosReducer = createReducer(initialState, {
+  [todosActionsTypes.ADD_TODO]: (state, action) =>
+    state.concat({
+      id: action.payload.id,
+      text: action.payload.text,
+      completed: false,
+    }),
+  [todosActionsTypes.TOGGLE_TODO]: (state, action) =>
+    state.map((todo) => {
+      if (todo.id !== action.payload.id) {
+        return todo;
+      }
+      return {
+        ...todo,
+        completed: !todo.completed,
+      };
+    }),
+});
+
+/*
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case todosActionsTypes.ADD_TODO:
@@ -24,3 +44,6 @@ export default function todosReducer(state = initialState, action) {
       return state;
   }
 }
+*/
+
+export default todosReducer;
--- src/redux/visibility-filter/index.js
+++ src/redux/visibility-filter/index.js
@@ -1,20 +1,23 @@
-import * as filterActions from "./filterActionTypes";
+import * as filterActionsTypes from "./filterActionTypes";
 import * as filterCategories from "./filterCategories";
+import createReducer from "../createReducer";
 
 export const initialState = filterCategories.SHOW_ALL;
 
+const visibilityReducer = createReducer(initialState, {
+  [filterActionsTypes.SET_VISIBILITY_FILTER]: (state, action) =>
+    action.payload.filter,
+});
+
+/*
 export default function visibilityReducer(state = initialState, action) {
   switch (action.type) {
     case filterActions.SET_VISIBILITY_FILTER:
-      /**if (action.payload.filter === filterCategories.SHOW_UNDONE) {
-        return filterCategories.SHOW_UNDONE;
-      } else if (action.payload.filter === filterCategories.SHOW_DONE) {
-        return filterCategories.SHOW_DONE;
-      } else {
-        return filterCategories.SHOW_ALL;
-      }*/
       return action.payload.filter;
     default:
       return state;
   }
 }
+*/
+
+export default visibilityReducer;
