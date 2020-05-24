0018bfd0da41b1c02e1a1460164ccf381d1c81de
Caio César Silva <caiocesarsilva08@gmail.com>
2020-05-24 15:27:52 -0300

    Filter Todos
    Implemented the filter for the todo items, inside the function there's 3 ways to write it.

--- src/components/todosList/index.js
+++ src/components/todosList/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as todosActions from "../../redux/todos/todosActionsCreators";
+import * as filterCategories from "../../redux/visibility-filter/filterCategories";
 
-export const TodosList = ({ todos, handleToggleTodo }) => (
+export const TodosList = ({ todos, handleToggleTodo, activeFilter }) => (
   <ul>
-    {todos.map((todo) => (
+    {getVisibleTodos(todos, activeFilter).map((todo) => (
       <li
         onClick={handleToggleTodo(todo.id)}
         key={todo.id}
@@ -16,8 +17,39 @@ export const TodosList = ({ todos, handleToggleTodo }) => (
   </ul>
 );
 
+function getVisibleTodos(todos, activeFilter) {
+  /*
+  const filterItems = {
+    [filterCategories.SHOW_ALL]: todos,
+    [filterCategories.SHOW_DONE]: todos.filter((todo) => todo.completed),
+    [filterCategories.SHOW_UNDONE]: todos.filter((todo) => !todo.completed),
+  };
+  return filterItems[activeFilter];
+  */
+
+  //Here, we're doing the same thing as above
+  return {
+    [filterCategories.SHOW_ALL]: todos,
+    [filterCategories.SHOW_DONE]: todos.filter((todo) => todo.completed),
+    [filterCategories.SHOW_UNDONE]: todos.filter((todo) => !todo.completed),
+  }[activeFilter];
+
+  /* We can also write this function using a switch.
+  switch (activeFilter) {
+    case filterCategories.SHOW_DONE:
+      return todos.filter((todo) => todo.completed);
+    case filterCategories.SHOW_UNDONE:
+      return todos.filter((todo) => !todo.completed);
+    //The default case will also serve for the SHOW_ALL filter.
+    default:
+      return todos;
+  }
+  */
+}
+
 const mapStateToProps = (state) => ({
   todos: state.todos,
+  activeFilter: state.visibilityFilter,
 });
 
 const mapDispatchToProps = (dispatch) => ({
